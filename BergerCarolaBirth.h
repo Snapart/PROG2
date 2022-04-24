1c5de0e7ae860bfb5a55f40f0bb66772971fe3ac
Snapart <42581791+Snapart@users.noreply.github.com>
2022-04-24 15:46:57 +0200

    Delete BergerCarolaBirth.h

--- BergerCarolaBirth.h
+++
@@ -1,8 +0,0 @@
-// Carola Berger
-// BergerCarolaBirth.h
-// Nachname und Geburtsdatum ausgeben
-
-
-void BergerCarolaBirthDate(void); // ist die Deklaration für die Verwendung der Funktion in anderen Programmen
-
- 
